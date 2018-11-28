71a89762b12ceeec6e82eda8a2dfd7ba6e0851dc
smilu97 <smilup2244@gmail.com>
2018-11-28 11:06:39 +0900

    show x2

--- main.py
+++ main.py
@@ -5,8 +5,22 @@
 # Visualization of comparison between SGD and Adam
 #
 
+import matplotlib as mpl
+mpl.use('tkagg')
+import matplotlib.pyplot as plt
+import numpy as np
+
+def func_x2(x):
+    return x**2
+
+def draw_func(f, xrange):
+    x = np.arange(xrange[0], xrange[1], 0.1)
+    y = f(x)
+    plt.plot(x, y)
+
 def main():
-    pass
+    draw_func(func_x2, (-10.0, 10.0))
+    plt.show()
 
 if __name__ == '__main__':
     main()
